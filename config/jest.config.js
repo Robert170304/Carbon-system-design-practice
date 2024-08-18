47b0d10762571e85635ac3b01b6a6648f453c1a2
robertmacwan17 <robert.macwan@groovyweb.co>
2024-08-18 16:40:46 +0530

    FIX: test run issue while running pipeline

--- config/jest.config.js
+++ config/jest.config.js
@@ -2,8 +2,11 @@ module.exports = {
   rootDir: "../",
   setupFilesAfterEnv: ["./config/jest.setup.js"],
   transform: {
-    "\\.js$": ["babel-jest", { configFile: "./config/.babelrc" }],
+    "^.+\\.js$": ["babel-jest", { configFile: "./config/.babelrc" }],
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!string-width|cliui)", // Add any other modules that need to be transformed
+  ],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
